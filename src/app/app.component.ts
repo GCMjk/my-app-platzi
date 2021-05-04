d4b3719ce58d934ef2b117991fbc37a21d7c2be0
Martin <martingarnica.c@outlook.com>
2021-05-04 17:02:00 -0500

    Estructuras de control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app-platzi';
+
+  items = ['Martin', 'Janet', 'Nombres'];
+
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Prenda',
+      image: 'assets/images/camiseta.png',
+      price: 2000,
+      description: 'Descripcion del producto'
+    },
+    {
+      id: '2',
+      name: 'Prenda',
+      image: 'assets/images/hoodie.png',
+      price: 2100,
+      description: 'Descripcion del producto'
+    },
+    {
+      id: '2',
+      name: 'Prenda',
+      image: 'assets/images/mug.png',
+      price: 2200,
+      description: 'Descripcion del producto'
+    },
+    {
+      id: '3',
+      name: 'Prenda',
+      image: 'assets/images/pin.png',
+      price: 2300,
+      description: 'Descripcion del producto'
+    },
+    {
+      id: '4',
+      name: 'Prenda',
+      image: 'assets/images/stickers1.png',
+      price: 2400,
+      description: 'Descripcion del producto'
+    },{
+      id: '5',
+      name: 'Prenda',
+      image: 'assets/images/stickers2.png',
+      price: 2500,
+      description: 'Descripcion del producto'
+    },
+  ];
+
+  addItem() {
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number){
+    this.items.splice(index, 1);
+  }
 }
